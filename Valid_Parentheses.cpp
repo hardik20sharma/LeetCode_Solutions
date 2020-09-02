3f0221d498cf01e72973c2e519f19b2171b53481
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-09-02 10:26:58 +0530

    C++ code

---
+++ Valid_Parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string str)
+    {
+        stack<char> s;
+
+        for(char i : str)
+            if(i == '(' || i == '{' || i == '[')
+                s.push(i);
+            else if(i == ')' && s.size()!=0 && s.top() == '(')
+                s.pop();
+            else if(i == '}' && s.size()!=0 && s.top() == '{')
+                s.pop();
+            else if(i == ']' && s.size()!=0 && s.top() == '[')
+                s.pop();
+            else
+                return false;
+
+        return s.size() == 0;
+    }
+};
