00d3f5549f7b0a5fe52df615f63ca932b62a7827
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-09-08 15:04:24 +0530

    C++ code

--- Roman_to_Integer.cpp
+++ Roman_to_Integer.cpp
@@ -10,12 +10,12 @@ public:
         m.insert( pair<char, int> {'C', 100} );
         m.insert( pair<char, int> {'D', 500} );
         m.insert( pair<char, int> {'M', 1000} );
-        
+
         int result = 0;
-        
+
         for(int i=0; i<s.length(); i++)
             (m[s[i]] >= m[s[i+1]]) ? result += m[s[i]] : result -= m[s[i]];
-        
+
         return result;
     }
 };
