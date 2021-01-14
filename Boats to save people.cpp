621f535ec668b32ff72111aad6f195610a41bff3
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2021-01-14 09:47:47 +0530

    Create Boats to save people.cpp

---
+++ Boats to save people.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+
+        int i = 0, j = people.size()-1, res = 0;
+
+        while(i <= j)
+        {
+            res++;
+            if(people[i] + people[j] <= limit)
+                i++;
+            j--;
+        }
+
+        return res;
+    }
+};
