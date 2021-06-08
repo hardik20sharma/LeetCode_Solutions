43b952ab66ec8164c8279006a34a7c1a82f959f8
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2021-06-08 18:19:10 +0530

    Create Gray Code.cpp

---
+++ Gray Code.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    void backtrack(bitset<32> &bits, vector<int> &res, int n)
+    {
+        if(n == 0)
+        {
+            res.push_back(bits.to_ulong());
+            return;
+        }
+        
+        backtrack(bits, res, n-1);
+        bits.flip(n-1);
+        backtrack(bits, res, n-1);
+    }
+    
+    vector<int> grayCode(int n)
+    {
+        bitset<32> bits;
+        vector<int> res;
+        
+        backtrack(bits, res, n);
+        
+        return res;
+    }
+};
