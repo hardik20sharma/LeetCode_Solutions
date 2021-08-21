f7c953a8b221398469678e2efda2d6fc20dab046
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2021-08-21 10:48:27 +0530

    Update Valid Sudoku.cpp

--- Valid Sudoku.cpp
+++ Valid Sudoku.cpp
@@ -52,3 +52,83 @@ public:
     4,1     4,4     4,7
     7,1     7,4     7,7
 */
+
+/* ============================================================================================================================================= */
+/* ============================================================================================================================================= */
+/* -------------------------------------------------------- ANOTHER APPROACH ------------------------------------------------------------------- */
+/* ============================================================================================================================================= */
+/* ============================================================================================================================================= */
+
+class Solution {
+
+    int numInCell(vector<vector<char>> &board, int row, int col)
+    {
+        if(board[row][col] == '.')
+            return 0;
+
+        return (board[row][col] - '0');
+    }
+
+    bool isValidRow(vector<vector<char>> &board, int row)
+    {
+        vector<bool> store(9, false);
+
+        for(int j = 0; j < 9; j++)
+            if(numInCell(board, row, j))
+                if(store[ numInCell(board, row, j) ] == true)
+                    return false;
+                else
+                    store[ numInCell(board, row, j) ] = true;
+
+        return true;
+    }
+
+    bool isValidCol(vector<vector<char>> &board, int col)
+    {
+        vector<bool> store(9, false);
+
+        for(int i = 0; i < 9; i++)
+            if(numInCell(board, i, col))
+                if(store[ numInCell(board, i, col) ] == true)
+                    return false;
+                else
+                    store[ numInCell(board, i, col) ] = true;
+
+        return true;
+    }
+    
+    bool isValidBox(vector<vector<char>> &board, int row, int col)
+    {
+        vector<bool> store(9, false);
+        
+        for(int i = row-1; i <= row+1; i++)    
+            for(int j = col-1; j <= col+1; j++)
+                if(numInCell(board, i, j))
+                    if(store[ numInCell(board, i, j) ] == true)
+                        return false;
+                    else
+                        store[ numInCell(board, i, j) ] = true;
+        
+        return true;
+    }
+
+public:
+
+    bool isValidSudoku(vector<vector<char>>& board)
+    {
+        for(int i = 1; i < 9; i+= 3)
+            for(int j = 1; j < 9; j+=3)
+                if(isValidBox(board, i, j) == false)
+                    return false;
+        
+        for(int i = 0; i < 9; i++)
+            if(isValidRow(board, i) == false)
+                return false;
+        
+        for(int j = 0; j < 9; j++)
+            if(isValidCol(board, j) == false)
+                return false;
+
+        return true;
+    }
+};
